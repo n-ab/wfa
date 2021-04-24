cebd9db0adcd7b8afd7c1cbab19b943602016445
n-ab <32076158+n-ab@users.noreply.github.com>
2021-04-23 22:09:44 -0500

    Sound List css redo.

--- client/src/app/components/soundlist/soundlist.component.ts
+++ client/src/app/components/soundlist/soundlist.component.ts
@@ -11,10 +11,9 @@ import { SoundService } from 'src/app/services/sound.service';
 })
 export class SoundlistComponent implements OnInit, AfterViewInit {
 
+  admin = false;
+
   searchByNameForm: FormGroup;
-  searchByKeywordForm: FormGroup;
-  searchByLibraryForm: FormGroup;
-  searchByAnyForm: FormGroup;
   validSearchEntries: string[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-'];
 
   soundsWereFound = false;
@@ -30,9 +29,6 @@ export class SoundlistComponent implements OnInit, AfterViewInit {
 
   constructor(private soundService: SoundService) {
     this.searchByNameForm = new FormGroup({ query: new FormControl(null) });
-    this.searchByKeywordForm = new FormGroup({ query: new FormControl(null) });
-    this.searchByLibraryForm = new FormGroup({ query: new FormControl(null) });
-    this.searchByAnyForm = new FormGroup({ query: new FormControl(null) });
   }
 
   ngOnInit(): void {
@@ -60,27 +56,20 @@ prepNameSearchBarr(): void {
 }
 
 handleValidEntry(eventKey: string): void {
-  console.log('++ HANDLING INPUT ...');
   if (this.searchQueryArray.length === 0) {
-    // user started searching for something
     this.searchQueryArray.push(eventKey);
     this.mapSounds();
   } else {
-    // user added to search query
     this.searchQueryArray[0] += eventKey;
     this.mapSounds();
   }
 }
 
 mapSounds(): void {
-  this.soundList.map((sound, index) => {
-    if (this.testRegex(sound)) { this.putSoundInQueue(sound); }
-  });
-  console.log('sound queue ', this.soundListQueue);
+  this.soundList.map((sound, index) => { if (this.testRegex(sound)) { this.putSoundInQueue(sound); } });
   this.newSoundList.length = 0;
   this.newSoundList.push(...this.soundListQueue);
   this.soundListQueue.length = 0;
-  console.log('new sound list', this.newSoundList);
   this.showFullSoundList = false;
   this.showFilteredSoundList = true;
 }
@@ -115,21 +104,6 @@ handleBackspace(): void {
     const form = this.searchByNameForm.getRawValue();
     // console.log('attempting to search with: ', form + ' with \'name\' filter.');
     return this.search(form, 'name');
-  }
-
-  searchByKeyword(): void {
-    const form = this.searchByKeywordForm.getRawValue();
-    return this.search(form, 'keyword');
-  }
-
-  searchByLibrary(): void {
-    const form = this.searchByLibraryForm.getRawValue();
-    return this.search(form, 'library');
-  }
-
-  searchByAny(): void {
-    const form = this.searchByAnyForm.getRawValue();
-    return this.search(form, 'any');
   }
 
   search(query: string, filter: string): void {
